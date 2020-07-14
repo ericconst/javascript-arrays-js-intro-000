8c06d2ff799ba839140f5dddfd23a425073e2e3e
Eric C <erix2205@gmail.com>
2020-07-14 10:28:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,11 @@
 var chocolateBars = [ "snickers" , "hundred grand" , "kitkat" , "skittles"]
 
 function destructivelyAddElementToBeginningOfArray(array,n) {
-  array = array.shift(n)
+  var array = array.shift(n)
   return array
 }
 
 function addElementToBeginningOfArray(array, n) {
-  return [...n, array]
+  var newArray = [...n, array]
+  return newArray
 }
